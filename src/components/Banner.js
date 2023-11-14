1fd70f38873b2faf4aa410efc00015cb032acf31
Barrientos, Glenn F. <glenn.f.barrientos@accenture.com>
2023-11-14 19:37:13 +0800

    Home Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,112 @@
 import React from 'react';
 
+// images
+import Image from '../assets/avatar.svg';
+// icons
+import { FaGithub, FaYoutube, FaDribbble, FaFacebook } from 'react-icons/fa';
+// type animation
+import { TypeAnimation } from 'react-type-animation';
+// motion
+import { motion } from 'framer-motion';
+// variants
+import { fadeIn } from '../variants';
+
 const Banner = () => {
   return (
-    <div className='section' id='home'>
-      Banner
-    </div>
+    <section
+      className='min-h-[85vh] lg:min-h-[78vh] flex items-center'
+      id='home'
+    >
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+          {/* text */}
+          <div className='flex-1 text-center font-secondary lg:text-left'>
+            <motion.h1
+              variants={fadeIn('up', 0.3)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='text-[55px] font-bold leading-[0.8] lg:text-[70px]'
+            >
+              GLENN <span>BARRIENTOS</span>
+            </motion.h1>
+            <motion.div
+              variants={fadeIn('up', 0.4)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-6 text-[36px] lg:text-[40px] font-secondary font-semibold uppercase leading-[1]'
+            >
+              <span className='text-white mr-4'>I am a</span>
+              <TypeAnimation
+                sequence={[
+                  2000,
+                  'React Developer',
+                  2000,
+                  'Angular Developer',
+                  2000,
+                ]}
+                speed={50}
+                className='text-accent'
+                wrapper='span'
+                repeat={Infinity}
+                preRenderFirstString={true}
+              />
+            </motion.div>
+            <motion.p
+              variants={fadeIn('up', 0.5)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'
+            >
+              I consider myself as a Web App Developer with over 12 years of
+              combined experience in Web based Application Development.
+              Throughout my career, I've had the opportunity to work at
+              different companies and have worked on techologies such as
+              ASP.NET, MVC, C#, Angular and React.
+            </motion.p>
+            <motion.div
+              variants={fadeIn('up', 0.6)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'
+            >
+              <button className='btn btn-lg'>Contact Me</button>
+              <a href='#' className='text-gradient btn-link'>
+                My Portfolio
+              </a>
+            </motion.div>
+            <motion.div
+              variants={fadeIn('up', 0.7)}
+              initial='hidden'
+              whileInView={'show'}
+              viewport={{ once: false, amount: 0.7 }}
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'
+            >
+              {/* socials */}
+              <a href='#'>
+                <FaGithub />
+              </a>
+              <a href='#'>
+                <FaFacebook />
+              </a>
+            </motion.div>
+          </div>
+          {/* image */}
+          <motion.div
+            variants={fadeIn('down', 0.7)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.7 }}
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[582px]'
+          >
+            <img src={Image} alt='' srcset='' />
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
 
