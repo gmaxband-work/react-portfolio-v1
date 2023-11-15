9d6ff883a2d34d2a217f12de0f210836f1a800e8
Barrientos, Glenn F. <glenn.f.barrientos@accenture.com>
2023-11-15 16:32:37 +0800

    About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,79 @@
 import React from 'react';
 
+// countup
+import CountUp from 'react-countup';
+// intersection observer hook
+import { useInView } from 'react-intersection-observer';
+// motion
+import { motion } from 'framer-motion';
+//variant
+import { fadeIn } from '../variants';
+
 const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
   return (
-    <div className='section' id='about'>
-      About
-    </div>
+    <section className='section' id='about' ref={ref}>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-0 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+          {/* image */}
+          <motion.div
+            variants={fadeIn('right', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex lg:flex-1 bg-about bg-contain bg-no-repeat h-[450px] lg:h-[700px] mix-blend-lighten bg-top mb-10 lg:mb-0'
+          ></motion.div>
+          {/* text */}
+          <motion.div
+            variants={fadeIn('left', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 justify-center lg:justify-start'
+          >
+            <h2 className='h2 text-accent'>About me.</h2>
+            <h3 className='h3 mb-4'>
+              I'm a Front-end Developer with over 10+ years of experience.
+            </h3>
+            <p className='mb-6'>
+              Currently what I'm truly passionate about is on the newer Front
+              End technologies such as React and Angular. I find great
+              satisfaction in completing projects related to those front end
+              technologies where I think where my current strengths are.
+            </p>
+            {/* stats */}
+            <div className='flex gap-x-8 lg:gap-x-10 mb-12 justify-center lg:justify-start'>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {inView ? <CountUp start={0} end={10} duration={3} /> : null}+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Years of <br />
+                  Experience
+                </div>
+              </div>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {inView ? <CountUp start={0} end={50} duration={3} /> : null}+
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Projects <br />
+                  Completed
+                </div>
+              </div>
+            </div>
+            <div className='flex lg:max-w-max gap-x-8 items-center justify-center lg:justify-start'>
+              <button className='btn btn-lg'>Contact Me</button>
+              <a href='#' className='text-gradient btn-link'>
+                My Portfolio
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,7 +11,7 @@ import { Link } from 'react-scroll';
 
 const Nav = () => {
   return (
-    <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
+    <nav className='fixed bottom-10 lg:bottom-8 w-full overflow-hidden z-50'>
       <div className='container mx-auto'>
         <div className='w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-10 flex justify-between items-center text-2xl text-white/50'>
           <Link
